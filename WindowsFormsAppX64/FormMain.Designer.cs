3c1d77cde6818c9db2f01fb57ead7a99a7c57189
Fred <domaine75@gmail.com>
2021-04-21 15:55:03 +0200

    Ajoutez des fichiers projet.

---
+++ WindowsFormsAppX64/FormMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WindowsFormsAppX64
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.labelRamUsed = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // labelRamUsed
+      // 
+      this.labelRamUsed.AutoSize = true;
+      this.labelRamUsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.labelRamUsed.Location = new System.Drawing.Point(27, 37);
+      this.labelRamUsed.Name = "labelRamUsed";
+      this.labelRamUsed.Size = new System.Drawing.Size(247, 24);
+      this.labelRamUsed.TabIndex = 0;
+      this.labelRamUsed.Text = "This application is using  ";
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(547, 105);
+      this.Controls.Add(this.labelRamUsed);
+      this.Name = "FormMain";
+      this.Text = "App X64";
+      this.Load += new System.EventHandler(this.FormMain_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label labelRamUsed;
+  }
+}
+
---
+++ WindowsFormsAppX64/FormMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppX64
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    string chaine;
+    int dimension = 1000;
+    string[,,] tableau = new string[200, 1000, 1000];
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+      string alphabet = "abcdefghijklmnopqrstuvwxyz";
+      for (int i = 0; i < 100000; i++)
+      {
+        chaine += alphabet;
+      }
+
+      for (int i = 0; i < 200; i++)
+      {
+        for (int j = 0; j < dimension; j++)
+        {
+          for (int k = 0; k < dimension; k++)
+          {
+            tableau[i, j, k] = chaine;
+          }
+        }
+      }
+
+      Process proc = Process.GetCurrentProcess();
+      labelRamUsed.Text = $"This application is using  {SizeSuffix(proc.WorkingSet64)}";
+    }
+
+    static string SizeSuffix(long value, int decimalPlaces = 1)
+    {
+      string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+      if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException(nameof(decimalPlaces)); }
+      if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
+      if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
+
+      // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
+      int mag = (int)Math.Log(value, 1024);
+
+      // 1L << (mag * 10) == 2 ^ (10 * mag) 
+      // [i.e. the number of bytes in the unit corresponding to mag]
+      decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+
+      // make adjustment when the value is large enough that
+      // it would round up to 1000 or more
+      if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
+      {
+        mag += 1;
+        adjustedSize /= 1024;
+      }
+
+      return string.Format("{0:n" + decimalPlaces + "} {1}", adjustedSize, SizeSuffixes[mag]);
+    }
+  }
+}
