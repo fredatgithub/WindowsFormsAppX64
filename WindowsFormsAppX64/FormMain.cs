73fb22922d7091dcc17972ffadfc98ba61fae9a4
Fred <domaine75@gmail.com>
2021-04-25 17:06:50 +0200

    clean code

--- WindowsFormsAppX64/FormMain.cs
+++ WindowsFormsAppX64/FormMain.cs
@@ -38,29 +38,29 @@ namespace WindowsFormsAppX64
       labelRamUsed.Text = $"This application is using  {SizeSuffix(proc.WorkingSet64)}";
     }
 
-    static string SizeSuffix(long value, int decimalPlaces = 1)
+    private static string SizeSuffix(long value, int decimalPlaces = 1)
     {
       string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
       if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException(nameof(decimalPlaces)); }
       if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
       if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
 
-      // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
-      int mag = (int)Math.Log(value, 1024);
+      // magnitude is 0 for bytes, 1 for KB, 2, for MB, etc.
+      int magnitude = (int)Math.Log(value, 1024);
 
       // 1L << (mag * 10) == 2 ^ (10 * mag) 
       // [i.e. the number of bytes in the unit corresponding to mag]
-      decimal adjustedSize = (decimal)value / (1L << (mag * 10));
+      decimal adjustedSize = (decimal)value / (1L << (magnitude * 10));
 
       // make adjustment when the value is large enough that
       // it would round up to 1000 or more
       if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
       {
-        mag += 1;
+        magnitude += 1;
         adjustedSize /= 1024;
       }
 
-      return string.Format("{0:n" + decimalPlaces + "} {1}", adjustedSize, SizeSuffixes[mag]);
+      return string.Format("{0:n" + decimalPlaces + "} {1}", adjustedSize, SizeSuffixes[magnitude]);
     }
   }
 }
